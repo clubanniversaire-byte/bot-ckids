cacdfd2648fef8cf3a3ce13dccc33d60d6d76fe5
clubanniversaire-byte <clubanniversaire@tsivothachem.fr>
2026-04-29 10:12:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 # הגדרות - המערכת תמשוך את המידע מה-Environment Variables ב-Render
 ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
-PHONE_NUMBER_ID = "1000407146489466"
+PHONE_NUMBER_ID = "1089400387587213"
 VERIFY_TOKEN = os.environ.get("VERIFY_TOKEN", "MY_SECRET_TOKEN_123")
 GOOGLE_SHEET_URL = "https://script.google.com/macros/s/AKfycbzqQgKCbf88Um6zNjNkVZoaxNwk3Qfa_R3ffqVFD7LbIP2GjZvS9c5o05J_27DJvWu_/exec"
 
